207567e8bac12b796109fb8cd23f47bab77f292f
muhaabid <muhaabid11@gmail.com>
2024-03-20 14:19:19 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi fuga quibusdam nisi accusamus? Consequuntur quidem, ut quisquam vero eveniet recusandae culpa vitae eligendi sint et, nulla, soluta tempore atque ea!</p>
 </body>
 </html>
